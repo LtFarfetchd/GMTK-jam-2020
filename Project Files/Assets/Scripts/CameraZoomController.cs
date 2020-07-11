d30fc5b0e2b6f66a96892d3bfe064027cd39cd1a
Adam Judd <adam.judd@rea-group.com.au>
2020-07-11 23:28:15 +1000

    Facilitating lerped camera zoom during engagement

--- Project Files/Assets/Scripts/CameraZoomController.cs
+++ Project Files/Assets/Scripts/CameraZoomController.cs
@@ -2,25 +2,29 @@
 
 public class CameraZoomController : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject house;
+    public Camera thisCamera;
+    public float targetSize;
+
     private enum State {
         ZOOMING,
         RETURNING,
         STATIC
     }
     private State state;
-    private Vector3 cameraStart;
+    private Vector3 startPosition;
+    private float startSize;
     private Vector2 path;
     private Vector2 playerStart;
     private Vector2 playerTarget;
     private float playerXTarget, playerYTarget;
-    public GameObject player;
-    public GameObject house;
 
     public void ZoomIn(Vector2 playerStart, Vector2 playerTarget) 
     {
         this.playerStart = playerStart;
         this.playerTarget = playerTarget;
-        path = playerTarget - (Vector2)cameraStart;
+        path = playerTarget - (Vector2)startPosition;
         state = State.ZOOMING;
     }
 
@@ -29,7 +33,8 @@ public class CameraZoomController : MonoBehaviour
     void Start()
     {
         state = State.STATIC;
-        cameraStart = transform.position;
+        startPosition = transform.position;
+        startSize = thisCamera.orthographicSize;
     }
 
     void Update()
@@ -40,10 +45,12 @@ public class CameraZoomController : MonoBehaviour
                 Vector2.Distance(playerStart, (Vector2)player.transform.position)
                 / Vector2.Distance(playerStart, playerTarget);
 
-            Vector2 newPos = Vector2.Lerp((Vector2)cameraStart, playerTarget, movementProportion);
-            transform.position = new Vector3(newPos.x, newPos.y, cameraStart.z);
+            Vector2 newPos = Vector2.Lerp((Vector2)startPosition, playerTarget, movementProportion);
+            transform.position = new Vector3(newPos.x, newPos.y, startPosition.z);
 
-            if ((Vector2)transform.position == (state == State.ZOOMING ? playerTarget : (Vector2)cameraStart))
+            thisCamera.orthographicSize = Mathf.Lerp(startSize, targetSize, movementProportion);
+
+            if ((Vector2)transform.position == (state == State.ZOOMING ? playerTarget : (Vector2)startPosition))
                 state = (state == State.ZOOMING ? State.RETURNING : State.STATIC);
         }
     }
