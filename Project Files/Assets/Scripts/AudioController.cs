bd46ac7413bbe7644f00f81a7521337d00299b62
Slowpoke4DeWin <45135256+Slowpoke4DeWin@users.noreply.github.com>
2020-07-12 15:05:25 +1000

    ajusting audio levels on the fly

--- Project Files/Assets/Scripts/AudioController.cs
+++ Project Files/Assets/Scripts/AudioController.cs
@@ -23,6 +23,34 @@ public class AudioController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-                
+        float temp = 0f;
+        float audioLayerPercent = 100/5;
+        if (temp < audioLayerPercent)
+        {
+            changeAudioVolume(baselineChatter, temp / audioLayerPercent);
+        }
+        else if(temp < audioLayerPercent *2)
+        {
+            changeAudioVolume(beerClink, (temp - audioLayerPercent) / audioLayerPercent);
+        }
+        else if(temp < audioLayerPercent *3)
+        {
+            changeAudioVolume(maxChatter, (temp - audioLayerPercent * 2) / audioLayerPercent);
+        }
+        else if(temp < audioLayerPercent *4)
+        {
+            changeAudioVolume(maxChatter, (temp - audioLayerPercent * 3) / audioLayerPercent);
+        }
+    }
+
+    private void changeAudioVolume(AudioClip cliptochange, float audiolevel = 0f)
+    {
+        foreach(AudioSource adsc in audioPlayer)
+        {
+            if (adsc.clip == cliptochange)
+            {
+                adsc.volume = audiolevel;
+            }
+        }
     }
 }
