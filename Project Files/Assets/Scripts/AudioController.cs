979ed337869e442344d05e73c0d607897f33448e
Slowpoke4DeWin <45135256+Slowpoke4DeWin@users.noreply.github.com>
2020-07-12 14:38:36 +1000

    basic audio setup

---
+++ Project Files/Assets/Scripts/AudioController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioClip backgroundMusic, baselineChatter, maxChatter, beerClink, cheers;
+    private AudioSource[] audioPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponents<AudioSource>();
+        foreach(AudioSource adsc in audioPlayer)
+        {
+            if (adsc.clip != backgroundMusic)
+            {
+                adsc.volume = 0f;
+            }
+            adsc.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                
+    }
+}
