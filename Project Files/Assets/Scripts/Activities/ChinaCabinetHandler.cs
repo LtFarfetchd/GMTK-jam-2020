28b6cdbba509be73cc72fd96cc735b7eb2e1dc04
Slowpoke4DeWin <45135256+Slowpoke4DeWin@users.noreply.github.com>
2020-07-13 00:09:14 +1000

    small beginins

--- Project Files/Assets/Scripts/Activities/ChinaCabinetHandler.cs
+++ Project Files/Assets/Scripts/Activities/ChinaCabinetHandler.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class ChinaCabinetHandler : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject player, china, hands;
+    public GameObject sceneCamera;
+    private Camera sceneCameraThing;
     private PlayerController playerController;
+    
 
     void Start()
     {
         playerController = (PlayerController)player.GetComponent<MonoBehaviour>();
+        sceneCameraThing = sceneCamera.GetComponent<Camera>();
     }
 
     void OnAwake()
@@ -17,9 +21,12 @@ public class ChinaCabinetHandler : MonoBehaviour
 
     void Update()
     {
+        Vector3 mousePoint = sceneCameraThing.ScreenToWorldPoint(Input.mousePosition);
+        hands.transform.position = new Vector3(mousePoint.x, hands.transform.position.y, hands.transform.position.z);
         // placeholder disengagement. The game should determine when to disengage according to game rules
         if (Input.GetMouseButtonDown(0))
         {
+            
             playerController.InitiateDisengagement();
         }
     }
