b32f0d5142bebfef273cc0ef86e511b85d0be913
Adam Judd <adam.judd@rea-group.com.au>
2020-07-18 18:27:25 +1000

    General refactoring of handler and pushing magic numbers out to inspector fields

--- Project Files/Assets/Scripts/Activities/BeerPongHandler.cs
+++ Project Files/Assets/Scripts/Activities/BeerPongHandler.cs
@@ -1,117 +1,121 @@
 using UnityEngine;
+using ActivityVariety = ActivitiesHandlerController.ActivityVariety;
+using Room = HouseController.Room;
 
 public class BeerPongHandler : MonoBehaviour
 {
-    public GameObject player, hand, ball, cupback, cupfront, background;
+    public GameObject player, house, hand, ball, cupback, cupfront, background;
+    public ActivityVariety activityVariety = ActivityVariety.OBLIGATION;
+    public Room room = Room.TOP_LEFT;
     public Sprite handBall, handNoBall;
+    public float handStartSpeed = 0.005f, handAcceleration = 0.0001f, 
+        ballStartSpeed = 0.1f, ballAcceleration = 0.0001f, ballCurve = 0.0025f;
+    public float victoryToleranceDistance = 1f;
     private PlayerController playerController;
-    private float throwDistance = 0f;
-    private float throwSpeed = 0.1f;
-    private float throwMax = 10000f;
-    private float throwRangeMin = 4000f;
-    private float throwRangeMax = 6000f;
-    private float animationChange = 0.0001f, animationStart;
-    private bool thrown = false;
-
+    private HouseController houseController;
+    private Vector3 handStartPos;
+    private float handSpeed, handMaximumX, ballSpeed, ballMaximumY;
+    private float winRangeMinX, winRangeMaxX;
+    private bool thrown = false, hasWon = false, isInitialised = false, ballResorted = false;;
+    private int ballStartSortingOrder;
 
     void Start()
     {
         playerController = (PlayerController)player.GetComponent<MonoBehaviour>();
-    }
-
-    void Awake()
-    {
-        throwDistance = 0f;
-        throwSpeed = 0.1f;
-        float thisx = this.transform.position.x;
-        float thisy = this.transform.position.y;
-        float cupoffset = 0.5f;
-        background.transform.position = new Vector3(thisx, thisy, background.transform.position.z);
-        hand.transform.position = new Vector3(thisx - 1.55f, thisy, hand.transform.position.z);
-        cupback.transform.position = new Vector3(thisx, thisy + cupoffset, cupback.transform.position.z);
-        cupfront.transform.position = new Vector3(thisx, thisy + cupoffset, cupfront.transform.position.z);
-        ball.transform.position = new Vector3(10, 10, -6);
-        hand.GetComponent<SpriteRenderer>().sprite = handBall;
-        thrown = false;
-        animationStart = 0.017f;
-        // reset the game's state here and prepare to start
+        houseController = (HouseController)house.GetComponent<MonoBehaviour>();
     }
 
     void Update()
     {
-        if (thrown == false)
-        {
-            if (Input.GetKeyDown("space"))
-            {
-                hand.GetComponent<SpriteRenderer>().sprite = handNoBall;
-                ball.transform.position = new Vector3(hand.transform.position.x, hand.transform.position.y - 0.5f, -6);
-                thrown = true;
-            }
+        if (!isInitialised)
+            Initialise();
 
-            if (throwDistance + throwSpeed < throwMax)
-            {
-                throwDistance += throwSpeed;
-                throwSpeed += 0.05f;            
-            }
-            else
-            {
-                throwDistance = throwMax;
-                thrown = true;
-            }
-
-            float newx = this.transform.position.x + ((throwDistance*2f/throwMax - 1f) * 1.55f);
-            hand.transform.position = new Vector3( newx, hand.transform.position.y, hand.transform.position.z);
-
-        }
+        if (thrown)
+            MoveBall(hasWon);
         else
-        {
-            if(throwDistance >= throwRangeMin && throwDistance <= throwRangeMax)
-            {
-                WinAnimation();
-            }
-            else
-            {
-                LoseAnimation();
-            }
-        }        
+            MoveHand();
     }
 
-    private void WinAnimation()
-    {        
-        if(ball.transform.position.x < cupback.transform.position.x)
-        {
-            ball.transform.position = new Vector3(ball.transform.position.x + 0.001f, ball.transform.position.y + animationStart, ball.transform.position.z);
-        }
-        else if(ball.transform.position.x > cupback.transform.position.x)
-        {
-            ball.transform.position = new Vector3(ball.transform.position.x - 0.001f, ball.transform.position.y + animationStart, ball.transform.position.z);
-        }
-        else
-        {
-            ball.transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y + animationStart, ball.transform.position.z);
-        }
-
-        if(animationStart <= 0)
-        {
-            ball.transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y, -4);
-        }
-
-        if (animationStart <= 0 && ball.transform.position.y <= cupback.transform.position.y)
-        {
-            Awake();
-            playerController.InitiateDisengagement();
-        }
-        animationStart -= animationChange;
-        //playerController.InitiateDisengagement();
-    }
-
-    private void LoseAnimation()
+    private void Reset()
     {
-        ball.transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y + animationStart, ball.transform.position.z);
-        if (animationStart <= 0 && ball.transform.position.y <= cupback.transform.position.y - 0.35f)
+        handSpeed = handStartSpeed;
+        hand.GetComponent<SpriteRenderer>().sprite = handBall;
+        hand.transform.position = handStartPos;
+        ball.SetActive(false);
+        ball.GetComponent<SpriteRenderer>().sortingOrder = ballStartSortingOrder;
+        thrown = false;
+        ballSpeed = ballStartSpeed;
+        ballResorted = false;
+        hasWon = false;
+    }
+
+    private void Initialise()
+    {
+        SpriteRenderer cupRenderer = cupback.GetComponent<SpriteRenderer>();
+        winRangeMinX = cupRenderer.bounds.min.x - victoryToleranceDistance;
+        winRangeMaxX = cupRenderer.bounds.max.x + victoryToleranceDistance;
+        ballMaximumY = cupRenderer.bounds.max.y + 
+            (cupRenderer.bounds.max.y - cupRenderer.bounds.min.y); 
+        SpriteRenderer backgroundRenderer = background.GetComponent<SpriteRenderer>();
+        SpriteRenderer handRenderer = hand.GetComponent<SpriteRenderer>();
+        handMaximumX = backgroundRenderer.bounds.max.x - 
+            (handRenderer.bounds.max.x - handRenderer.bounds.min.x);
+        handStartPos = hand.transform.position;
+        ballStartSortingOrder = ball.GetComponent<SpriteRenderer>().sortingOrder;
+        Reset();
+        isInitialised = true;
+    }
+
+    private void MoveHand()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            Awake();
+            hand.GetComponent<SpriteRenderer>().sprite = handNoBall;
+            ball.SetActive(true);
+            ball.transform.position = hand.transform.position;
+            thrown = true;
+            float handX = hand.transform.position.x;
+            hasWon = (handX >= winRangeMinX && handX <= winRangeMaxX);
         }
-        animationStart -= animationChange;
+        
+        handSpeed += handAcceleration;
+        float targetX = hand.transform.position.x + handSpeed;
+        if (targetX < handMaximumX)
+            hand.transform.position = new Vector3(targetX, hand.transform.position.y, hand.transform.position.z);
+    }
+
+    private void MoveBall(bool hasWon)
+    {        
+        if (hasWon)
+        {
+            float xOffsetFromCup = cupback.transform.position.x - ball.transform.position.x;
+            float xMove = ballCurve * (xOffsetFromCup != 0 ? Mathf.Sign(xOffsetFromCup) : 0f);
+            ball.transform.position = new Vector3(ball.transform.position.x + xMove, ball.transform.position.y + ballSpeed, ball.transform.position.z);
+            
+            if(!ballResorted && ballSpeed <= 0)
+            {
+                ballResorted = true;
+                int cupBackSortingOrder = cupback.GetComponent<SpriteRenderer>().sortingOrder;
+                int cupFrontSortingOrder = cupfront.GetComponent<SpriteRenderer>().sortingOrder;
+                // take the average of the sorting orders to guarantee the ball falls between the back and front
+                ball.GetComponent<SpriteRenderer>().sortingOrder = (int)(((float)(cupBackSortingOrder + cupFrontSortingOrder)) / 2f);
+            }
+
+            if (ballSpeed <= 0 && ball.transform.position.y <= cupback.transform.position.y)
+            {
+                Reset();
+                houseController.HealStat(activityVariety, room);
+                playerController.InitiateDisengagement();
+            }
+        }
+        else 
+        {
+            ball.transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y + ballSpeed, ball.transform.position.z);
+            if (ballSpeed <= 0 && ball.transform.position.y <= hand.transform.position.y)
+            {
+                Reset();
+            }
+        }
+        ballSpeed -= ballAcceleration;
     }
 }
